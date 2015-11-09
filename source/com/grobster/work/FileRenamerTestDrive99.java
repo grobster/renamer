3375079ae79f33c1c952009751e95811ffbb1033
grobster <jquarles@grobster.com>
2015-11-09 11:10:19 -0500

    new gui -- added the ability to zip log files older than 180 days

--- source/com/grobster/work/FileRenamerTestDrive99.java
+++ source/com/grobster/work/FileRenamerTestDrive99.java
@@ -10,7 +10,7 @@ public class FileRenamerTestDrive99 {
 		FileRenamerInterface renamer = new FileRenamerAllPhotos(new FileRenamerRevised(new FileRenamerPath()));
 		FileNameRecorderInterface recorder = new FileNameRecorder2(renamer);
 		//RenamerView11 view = new RenamerView11(renamer); commented on 7/22/14 to test new config view
-		RenamerViewConfig view = new RenamerViewConfig(renamer);
+		RenamerViewConfig2 view = new RenamerViewConfig2(renamer);
 		view.createView();
 
 			
